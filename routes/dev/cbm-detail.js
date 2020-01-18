b47c60b3ca14076369b5918d5d05d5c59c2a698c
Pablo Klaschka <contact@pabloklaschka.de>
2020-01-18 10:53:51 +0100

    Refactoring a little bit here and there

--- routes/dev/cbm-detail.js
+++ routes/dev/cbm-detail.js
@@ -1,35 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const releaseRouter = require('./release');
-const Release = require('../../model/Release');
 
 router.get('/', async (req, res) => {
     return res.render('dev/cbm/show', {cbm: req.cbm, releases: await req.cbm.getReleases(), latest: await req.cbm.getLatestApprovedRelease()});
 });
 
-router.get('/release/new', (req, res) => {
-    return res.render('dev/cbm/release/new', {
-        error: null
-    });
-});
-
-
-router.param('release', async (req, res, next, id) => {
-    try {
-        const release = await Release.get(Number.parseInt(id));
-
-        if (req['cbm'].id === (await release.getCBM()).id) {
-            req.release = release;
-            return next();
-        } else {
-            return res.sendStatus(403);
-        }
-    } catch (e) {
-        console.log('release not found');
-        return res.sendStatus(404);
-    }
-});
-
-router.use('/release/:release', releaseRouter);
+router.use('/release', releaseRouter);
 
 module.exports = router;
--- routes/dev/release.js
+++ routes/dev/release.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
-require('../../model/Release');
-router.get('/', async (req, res) => {
-    return res.render('dev/cbm/release/show', {...req.release});
+const Release = require('../../model/Release');
+
+router.get('/new', (req, res) => {
+    return res.render('dev/cbm/release/new', {
+        error: null
+    });
 });
 
+router.param('release', async (req, res, next, id) => {
+    try {
+        const release = await Release.get(Number.parseInt(id));
+
+        if (req['cbm'].id === (await release.getCBM()).id) {
+            req.release = release;
+            return next();
+        } else {
+            return res.sendStatus(403);
+        }
+    } catch (e) {
+        console.log('release not found');
+        return res.sendStatus(404);
+    }
+});
+
+router.get('/:release', async (req, res) => {
+    return res.render('dev/cbm/release/show', {...req.release});
+});
 
 module.exports = router;
