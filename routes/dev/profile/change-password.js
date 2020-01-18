14ff424dae4506433d0a1015e8b43ad05a857a81
Pablo Klaschka <contact@pabloklaschka.de>
2020-01-18 22:20:09 +0100

    More refactoring

---
+++ routes/dev/profile/change-password.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('dev/profile/change-password');
+});
+
+router.post('/', async (req, res) => {
+    let error = [];
+
+    if (!req.body['old'] || !await req.user.verifyPassword(req.body['old'])) {
+        error.push('Old password isn\'t correct');
+    }
+
+    if (!(req.body['new1'] && req.body['new2']) || req.body['new1'] !== req.body['new2']) {
+        error.push('Passwords don\'t match');
+    }
+
+    if (req.body['new1'].length < 4) {
+        error.push('Password must be at least 5 characters long')
+    }
+
+    if (error.length) {
+        return res.render('dev/profile/change-password', {error: error.join(', ')});
+    } else {
+        await req.user.setPassword(req.body['new1']);
+        await req.user.save();
+        return res.redirect('..');
+    }
+});
+
+module.exports = router;
---
+++ routes/dev/profile/edit.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+require('../../../model/Publisher');
+
+router.get('/', (req, res) => {
+    return res.render('dev/profile/edit', {user: req.user});
+});
+
+router.post('/', async (req, res) => {
+    if (!req.body['name'] || req.body.name.length < 3) {
+        return res.render('dev/profile/edit', {user: req.user, error: 'Username too short, must be >= 3 characters'});
+    } else {
+        req.user.name = req.body['name'];
+        await req.user.save();
+        return res.redirect('..');
+    }
+});
+
+module.exports = router;
