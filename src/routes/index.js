720c100d10000a9064da32cfe0207b7ba2833118
Pablo Klaschka <contact@pabloklaschka.de>
2020-04-12 04:46:19 +0200

    Some more frontend stuff

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Server State Plugin Registry' });
 });
 
+router.get('/legal-notice', ((req, res) => {
+  res.render('static/legal-notice');
+}))
+
+router.get('/privacy-policy', ((req, res) => {
+  res.render('static/privacy-policy');
+}))
+
 module.exports = router;
